5fc9ed28685957e01a300f53a2cb45f5c40b03ed
John Devaney <johndevaney1@hotmail.com>
2024-11-12 15:23:56 +0000

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,9 @@ import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    
     <>
       <MainNavigation />
-      <CoverImage
-        imageSrc="src/assets/images/cover.jpg"
-        altText="Book your appointment today"
-      />
+      <CoverImage altText="Book your appointment today" />
       <img
         className="cover-bottom-border"
         src="src/assets/images/banner-border.png"
@@ -41,8 +37,12 @@ function App() {
           }
         />
       </div>
-      <MapWithDescription lat={2} lng={2} description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
-      <Footer/>
+      <MapWithDescription
+        lat={2}
+        lng={2}
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      />
+      <Footer />
     </>
   );
 }
